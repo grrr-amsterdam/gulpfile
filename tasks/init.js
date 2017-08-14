ee4dd2827fdd59e334eed6ae0e470ff347526c16
Koen Schaft <koen@grrr.nl>
2017-08-14 13:54:27 +0200

    Added version output when starting Gulpfile

--- tasks/init.js
+++ tasks/init.js
@@ -1,12 +1,18 @@
 import { log } from './../lib/log';
 import { env } from '../lib/env';
 import gulp from 'gulp';
+import path from 'path';
+import fs from 'fs';
+
+const gulpfile = path.join(__dirname, '../package.json');
+const gulpfilePackage = JSON.parse(fs.readFileSync(gulpfile));
 
 /**
  * Shows a friendly message and does nothing else
  */
 gulp.task('init', () => {
-  log('----------------------------------');
+  log('───────────────────────────────────');
+  log(`Gulpfile: v${gulpfilePackage.version}`);
   log(`Environment: ${env}`);
-  log('----------------------------------');
+  log('───────────────────────────────────');
 });
