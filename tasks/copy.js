6cddbe85f9dd5be8806ee6f1faf842465c5c2075
Koen Schaft <koen@grrr.nl>
2017-09-18 14:15:28 +0200

    Fixed copy task not returning a proper finish

--- tasks/copy.js
+++ tasks/copy.js
@@ -6,10 +6,11 @@ import copy from 'gulp-copy';
 
 gulp.task('copy', (done) => {
   if (!config.has('tasks.copy')) {
-    return;
+    return done();
   }
   pump([
     gulp.src(config.get('tasks.copy')),
     copy(config.get('paths.dist'), { prefix: 1 }),
+    gulp.dest(config.get('paths.dist'))
   ], done);
 });
