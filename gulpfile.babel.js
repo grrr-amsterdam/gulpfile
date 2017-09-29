6c940e43bf7b4c0f3bcb209570a55d10df889d6e
Koen Schaft <koen@schoenkaft.com>
2017-09-29 15:31:33 +0200

    Added custom SIGINT event listener to exit process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 
-// Gulp tasks are defined in separate files in the tasks folder
+/**
+ * Gulp tasks are defined in separate files in the tasks folder
+ */
 const taskFolder = path.join(__dirname, 'tasks');
 fs.readdirSync(taskFolder)
   .filter((name) => /(\.(js)$)/i.test(path.extname(name)))
   .forEach((task) => require(path.join(taskFolder, task)));
+
+/**
+ * Custom `SIGINT` listener to exit process
+ */
+process.on('SIGINT', (e) => {
+  process.exit();
+});
