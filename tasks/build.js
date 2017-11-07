ce2696bc1513c4541ee17ce2288c0dbdb4410586
Koen Schaft <koen@schoenkaft.com>
2017-11-07 20:54:45 +0100

    Fixed commented build tasks

--- tasks/build.js
+++ tasks/build.js
@@ -4,18 +4,18 @@ import runSequence from 'run-sequence';
 gulp.task('build', (done) => runSequence.use(gulp)(
   'init',
   'clean',
-  // [
-  //   'modernizr',
-  //   'javascript:vendor',
-  //   'sass',
-  //   'copy',
-  //   'images',
-  //   'icons',
-  // ],
+  [
+    'modernizr',
+    'javascript:vendor',
+    'sass',
+    'copy',
+    'images',
+    'icons',
+  ],
   'javascript:build',
-  // 'eslint',
-  // 'sass:lint',
-  // 'revision',
+  'eslint',
+  'sass:lint',
+  'revision',
   done
 ));
 
