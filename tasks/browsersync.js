aa7b2c019abc6002218e7d8e0367d93f414eacf1
Martijn Gastkemper <martijn@grrr.nl>
2018-12-03 12:46:21 +0100

    Make Browsersync proxy configurable in `.env`

--- tasks/browsersync.js
+++ tasks/browsersync.js
@@ -3,9 +3,14 @@ import config from '../lib/config';
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 
-const domain = config.get('app.domain');
-const port = config.get('app.port') ? config.get('app.port') : null;
-const proxy = domain && port ? `${domain}:${port}` : domain;
+let proxy;
+if (process.env.BROWSERSYNC_PROXY) {
+  proxy = process.env.BROWSERSYNC_PROXY;
+} else {
+  const domain = config.get('app.domain');
+  const port = config.get('app.port') ? config.get('app.port') : null;
+  proxy = domain && port ? `${domain}:${port}` : domain;
+}
 
 /**
  * Auto refresh and hot reloading in the browser
