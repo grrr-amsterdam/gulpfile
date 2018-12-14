015f9a31178fc42d6ec6164f01a45cf3c6872dcb
Koen Schaft <koen@grrr.nl>
2018-12-14 15:35:06 +0100

    Add Browsersync examples and update dotenv cwd explicity

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
 import config from './lib/config';
 
-require('dotenv').config({path: config.get('dotenv.file')});
+/**
+ * Load environment variables from `.env`
+ */
+dotenv.config({ path: config.get('dotenv.file') || process.cwd() });
 
 /**
  * Gulp tasks are defined in separate files in the tasks folder
