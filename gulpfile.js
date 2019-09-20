7a19ca81b7c674b27df33aa30ce1e595f67144bd
Koen Schaft <koen@grrr.nl>
2019-09-20 10:09:49 +0200

    Add @grrr/utils and transform it in gulpfile and test task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ require('@babel/register')({
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-modules-commonjs',
   ],
-  only: [/(@grrr\/gulpfile|gulpfile)\/(gulpfile\.babel\.js|tasks|lib)/],
-  extensions: ['.js'],
+  only: [
+    /(@grrr\/gulpfile|gulpfile)\/(gulpfile\.babel\.js|tasks|lib)/,
+    /(@grrr\/utils)/,
+  ],
+  extensions: ['.js', '.mjs'],
 });
 require('./gulpfile.babel.js');
