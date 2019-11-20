e0518cdabb81a54a846a104eab97edf4fc4109d4
Koen Schaft <koen@grrr.nl>
2019-11-20 11:28:50 +0100

    Add json plugin to Rollup

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -9,6 +9,7 @@ import rollup from 'gulp-better-rollup';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import { src, dest, task } from 'gulp';
 
@@ -21,6 +22,7 @@ const generateBundle = ({ babelConfig, rollupConfig, bundleFile }, errorCallback
     .pipe(rollup({
       ...rollupConfig.input,
       plugins: [
+        json(),
         resolve(),
         commonjs(),
         babel(babelConfig),
