14dbb226442a6f59697a5b8916756378ffceda1a
Koen Schaft <koen@grrr.nl>
2020-02-28 18:06:31 +0100

    Allow Browsersync to be turned off

--- tasks/browsersync.js
+++ tasks/browsersync.js
@@ -1,3 +1,5 @@
+import config from '../lib/config';
+
 import browserSyncPackage from 'browser-sync';
 import { task } from 'gulp';
 
@@ -8,11 +10,14 @@ import { task } from 'gulp';
  * third party devices over the network.
  */
 export const browsersync = done => {
+  if (config.get('tasks.watch.browsersync') === false) {
+    return done();
+  }
   browserSyncPackage({
     proxy: process.env.BROWSERSYNC_PROXY,
     open: false,
   });
-  done();
+  return done();
 };
 
 export const reload = done => {
